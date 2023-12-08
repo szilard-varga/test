07e99f96b110356961ebf956b692e736bd1d63f2
Szilard Varga <136651660+szilard-varga@users.noreply.github.com>
2023-12-08 09:49:15 +0200

    Update SwapNumbers.java

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,7 +1,9 @@
 public class SwapNumbers {
 
+    // main
     public static void main(String[] args) {
 
+        // set some demo numbers
         float first = 1.20f, second = 2.45f;
 
         System.out.println("--Before swap--");
@@ -17,6 +19,7 @@ public class SwapNumbers {
         // Value of temporary (which contains the initial value of first) is assigned to second
         second = temporary;
 
+        // print out the results
         System.out.println("--After swap--");
         System.out.println("First number = " + first);
         System.out.println("Second number = " + second);
